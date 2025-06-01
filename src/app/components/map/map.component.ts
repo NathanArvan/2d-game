e756a6f2d02020a012b3b7f2af17fdc8c049baf7
Nathan Arvan <nathannarvan@gmail.com>
2025-06-01 11:13:00 -0500

    rewrite to use signals

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { Character } from '../../models/character.model';
 import { Item } from '../../models/item.model';
@@ -13,8 +13,25 @@ import { CharacterComponent } from "../character/character.component";
   styleUrl: './map.component.css'
 })
 export class MapComponent implements OnInit{
-  grid: (Character | Item | null)[][] = [];
+  characters = signal<Character[]>([]);
+  items = signal<Item[]>([]);
+  
   gridSize: number = 10; // Adjust as needed
+
+  
+  grid = computed<(Character | Item | null)[][]>(() => {
+    const grid = this.initializeGrid();
+
+    this.items().forEach(item => {
+      this.setCellItem(item.position.x, item.position.y, item);
+    });
+
+    this.characters().forEach(character => {
+      this.setCellCharacter(character.position.x, character.position.y, character);
+    });
+
+    return grid;
+  })
 
   constructor(private gameService: GameService) { }
 
@@ -23,55 +40,43 @@ export class MapComponent implements OnInit{
     this.loadGame();
   }
 
-  initializeGrid() {
+  initializeGrid(): any[] {
+    let grid = new Array(this.gridSize);
     for (let y = 0; y < this.gridSize; y++) {
-      this.grid[y] = [];
+      grid[y] = [];
       for (let x = 0; x < this.gridSize; x++) {
-        this.grid[y][x] = null;
+        grid[y][x] = null;
       }
     }
+    return grid;
   }
 
   loadGame() {
-    const characters = this.gameService.getCharacters();
-    characters.forEach(character => {
-      this.setCellCharacter(character.position.x, character.position.y, character);
-    });
+    this.characters.set(this.gameService.getCharacters());
 
-    const items =this.gameService.getItems();
-    items.forEach(item => {
-      this.setCellItem(item.position.x, item.position.y, item);
-    });
+    this.items.set(this.gameService.getItems());
+ 
   }
 
   getCellCharacter(x: number, y: number): Character | null {
-    return this.grid[y][x] as Character;
+    return this.grid()[y][x] as Character;
   }
 
   getCellItem(x: number, y: number): Item | null {
-    return this.grid[y][x] as Item;
+    return this.grid()[y][x] as Item;
   }
 
   setCellCharacter(x: number, y: number, character: Character) {
-    this.grid[y][x] = character;
+    this.grid()[y][x] = character;
   }
 
   setCellItem(x: number, y: number, item: Item) {
-    this.grid[y][x] = item;
+    this.grid()[y][x] = item;
   }
 
   moveCharacter(x: number, y: number) {
-  // const selectedCharacter = this.getCellCharacter(this.selectedX, this.selectedY);
-  // if (selectedCharacter && this.isWithinBounds(x, y)) {
-  //   this.gameService.moveCharacter(selectedCharacter.id, { x, y }).subscribe(() => {
-  //     // Update the character's position in the grid
-  //     this.setCellCharacter(this.selectedX, this.selectedY, null);
-  //     this.setCellCharacter(x, y, selectedCharacter);
-  //     this.selectedX = x;
-  //     this.selectedY = y;
-  //   });
-  // }
-}
+
+  }
 
 isWithinBounds(x: number, y: number): boolean {
   return x >= 0 && x < this.gridSize && y >= 0 && y < this.gridSize;
