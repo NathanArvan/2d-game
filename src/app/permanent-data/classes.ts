2434917f337878ee3ca6e7bf26def9199e9d9f4b
Nathan Arvan <nathannarvan@gmail.com>
2025-06-08 08:43:31 -0500

    create classes data file

---
+++ src/app/permanent-data/classes.ts
@@ -0,0 +1,29 @@
+import { Class } from "../models/class";
+
+export const Classes: Class[] = [
+    {
+        id: 1,
+        name: "Warrior",
+        actions: []
+    },
+    {
+        id: 2,
+        name: "Mage",
+        actions: []
+    },
+    {
+        id: 3,
+        name: "Rogue",
+        actions: []
+    },
+    {
+        id: 4,
+        name: "Ranger",
+        actions: []
+    },
+    {
+        id: 5,
+        name: "Bard",
+        actions: []
+    },
+]
