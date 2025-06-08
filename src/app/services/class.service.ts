29038de32aba0c00ad8a735be79ffc4182837a54
Nathan Arvan <nathannarvan@gmail.com>
2025-06-08 09:22:48 -0500

    class service with action and class look up methods

---
+++ src/app/services/class.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { ClassLevels } from '../models/character.model';
+import { ActionService } from './action.service';
+import { Classes } from '../permanent-data/classes';
+import { Class } from '../models/class';
+import { Action } from '../models/action';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassService {
+  private _classes = Classes;
+
+  constructor(private actionService: ActionService) { }
+
+  public getActionsForCharacterLevels(levels: ClassLevels[]): Action[] {
+    const actions: Action[] = [];
+    levels.forEach(level => {
+      const classLevel = level.level;
+      const playerClass = this.getClassById(level.classId);
+      if (playerClass !== null) {
+        const classActions = this.getUnlockedClassActions(playerClass, classLevel);
+        actions.push(...classActions);
+      }
+    });
+    return actions;
+  }
+
+  public getClassById(id: number): Class | null {
+    return this._classes.find(c => c.id === id) || null;
+  }
+
+  public getUnlockedClassActions(playerClass: Class, level: number): Action[] {
+    const unlockedActions: Action[] = [];
+    playerClass.actions.forEach(action => {
+      if (action.levelRestriction <= level) {
+        const act = this.actionService.getActionById(action.actionId);
+        if(act !== null)
+        unlockedActions.push(act);
+      }
+    });
+    return unlockedActions;
+  }
+}
