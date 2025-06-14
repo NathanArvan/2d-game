d4839516acbf21bd16d6e7d4c1410685b7f8db5c
Nathan Arvan <nathannarvan@gmail.com>
2025-06-14 08:42:57 -0500

    methods for attacking and drinking healing potion

--- src/app/services/attack.service.ts
+++ src/app/services/attack.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Attack } from '../models/action.model';
 import { Character } from '../models/character.model';
+import { DiceService } from './dice.service';
+import { DiceType } from '../models/dice.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttackService {
 
-  constructor() { }
+  constructor(private diceService: DiceService) { }
 
-  attackAgainstArmorClass(attack: Attack, target: Character): {updatedTarget : Character, message: string} {
-
-    const attackIsSuccessful = attack.calculatedIfSuccess(target);
+  attackWithWeapon(attack: Attack, target: Character): {updatedTarget : Character, message: string} {
+    const attackRoll = this.diceService.rollDie(DiceType.D20);
+    const armorClass = target.armorClass;
+    const attackIsSuccessful = attackRoll >= armorClass;
     if (!attackIsSuccessful) {
       return { updatedTarget: target, message: 'Attack failed' };
     }
-    // Calculate damage based on the attack's effectOnSuccess method
-    const damage = attack.effectOnSuccess();
+
+    const damage = this.diceService.rollDie(DiceType.D8);
     target.health = target.health - damage;
     return { updatedTarget: target, message: `You attacked ${target.name} for ${damage} damage` };
+  }
+  
+  drinkHealingPotion(target: Character): {updatedTarget : Character, message: string} {
+    const healing = this.diceService.rollDice(DiceType.D4, 2) + 2;
+    target.health += healing;
+    return { updatedTarget: target, message: `${target.name} drank a healing potion and gained ${healing} health` };
   }
 }
