d85ed1e71072bf86a29b10e47a9b02dd6d1ebdb9
Nathan Arvan <nathannarvan@gmail.com>
2025-06-14 09:16:30 -0500

    add effects to items

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,4 +1,5 @@
 import { Action } from "./action.model";
+import { DiceType } from "./dice.model";
 
 export interface ItemInstance extends ItemTemplate{
     id: number;
@@ -10,4 +11,18 @@ export interface ItemTemplate {
     actionIds: number[];
     name: string;
     description: string;
+    effect: Effect | null;
+}
+
+export interface Effect {
+    type: EffectType;
+    amount: {
+        diceCount: number;
+        diceType: DiceType;
+    }
+}
+
+export enum EffectType {
+    Damage,
+    Healing
 }
--- src/app/permanent-data/items.ts
+++ src/app/permanent-data/items.ts
@@ -1,4 +1,5 @@
-import { ItemTemplate } from "../models/item.model";
+import { DiceType } from "../models/dice.model";
+import { EffectType, ItemTemplate } from "../models/item.model";
 
 export const items: ItemTemplate[] = [
     {
@@ -6,17 +7,34 @@ export const items: ItemTemplate[] = [
         name: 'Potion',
         description: 'Heals the user for 50 health points.',
         actionIds: [4],
+        effect: {
+            type: EffectType.Healing,
+            amount: {
+                diceCount: 1,
+                diceType: DiceType.D8
+            }
+        }
+        
+
     },
     {
         templateId: 2,
         name: 'Sword',
         description: 'A sharp sword that deals damage to enemies.',
-        actionIds: [1]
+        actionIds: [1],
+        effect: {
+            type: EffectType.Damage,
+            amount: {
+                diceCount: 1,
+                diceType: DiceType.D8
+            }
+        }
     },
     {
         templateId: 3,
         name: 'Shield',
         description: 'Protects the user from incoming attacks.',
-        actionIds: [5]
+        actionIds: [5],
+        effect: null,
     },
 ]
