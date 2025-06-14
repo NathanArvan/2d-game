d53a0ee374a20e472c3257b1d26941baffc33dc0
Nathan Arvan <nathannarvan@gmail.com>
2025-06-14 09:25:26 -0500

    method for bare handed attack

--- src/app/services/attack.service.ts
+++ src/app/services/attack.service.ts
@@ -30,6 +30,20 @@ export class AttackService {
     return { updatedTarget: target, message: `You attacked ${target.name} for ${damage} damage` };
   }
 
+  attackBareHanded(target: Character): {updatedTarget : Character, message: string} {
+    const attackRoll = this.diceService.rollDie(DiceType.D20);
+    const armorClass = target.armorClass;
+    const attackIsSuccessful = attackRoll >= armorClass;
+    if (!attackIsSuccessful) {
+      return { updatedTarget: target, message: 'Attack failed' };
+    }
+    
+    const damage = this.diceService.rollDice(DiceType.D4, 1);
+    target.currentHealth = target.currentHealth - damage;
+    return { updatedTarget: target, message: `You attacked ${target.name} for ${damage} damage` };
+  }
+
+
   drinkHealingPotion(target: Character): {updatedTarget : Character, message: string} {
     const healing = this.diceService.rollDice(DiceType.D4, 2) + 2;
     target.health += healing;
