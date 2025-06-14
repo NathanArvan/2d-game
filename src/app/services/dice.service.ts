55d94c9206783d215a4ede1a0e2097271e52c610
Nathan Arvan <nathannarvan@gmail.com>
2025-06-14 08:34:52 -0500

    add methods to dice service

--- src/app/services/dice.service.ts
+++ src/app/services/dice.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DiceType } from '../models/dice.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 export class DiceService {
 
   constructor() { }
+
+  rollDice(type: DiceType, numberOfDice: number): number {
+    let total = 0;
+    for (let i = 0; i < numberOfDice; i++) {
+      total += this.rollDie(type);
+    }
+    return total;
+  }
+
+  rollDie(type: DiceType) {
+    return Math.floor(Math.random() * type) + 1;
+  }
+
+
 }
