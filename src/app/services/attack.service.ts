94462dc8f445e8198900f5db108a8971617196f6
Nathan Arvan <nathannarvan@gmail.com>
2025-06-14 10:53:22 -0500

    add more detail to attack messages

--- src/app/services/attack.service.ts
+++ src/app/services/attack.service.ts
@@ -13,34 +13,41 @@ export class AttackService {
   constructor(private diceService: DiceService) { }
 
   attackWithWeapon(weapon: ItemInstance, target: Character): {updatedTarget : Character, message: string} {
+    let message = '';
     const weaponEffect = weapon.effect;
     if (!weaponEffect || weaponEffect.type !== EffectType.Damage) {
       return { updatedTarget: target, message: 'Weapon does not have a damage effect' };
     }
-
     const attackRoll = this.diceService.rollDie(DiceType.D20);
     const armorClass = target.armorClass;
+    message = this.appendMessage(message, `Rolled a ${attackRoll} against targets armour class of ${armorClass}`)
     const attackIsSuccessful = attackRoll >= armorClass;
     if (!attackIsSuccessful) {
-      return { updatedTarget: target, message: 'Attack failed' };
+      message = this.appendMessage(message, 'Attack failed')
+      return { updatedTarget: target, message };
     }
     
     const damage = this.diceService.rollDice(weaponEffect.amount.diceType, weaponEffect.amount.diceCount);
     target.currentHealth = target.currentHealth - damage;
-    return { updatedTarget: target, message: `You attacked ${target.name} for ${damage} damage` };
+    message = this.appendMessage(message, `You attacked ${target.name} for ${damage} damage`);
+    return { updatedTarget: target, message };
   }
 
   attackBareHanded(target: Character): {updatedTarget : Character, message: string} {
+    let message = '';
     const attackRoll = this.diceService.rollDie(DiceType.D20);
     const armorClass = target.armorClass;
+    message = this.appendMessage(message, `Rolled a ${attackRoll} against targets armour class of ${armorClass}`)
     const attackIsSuccessful = attackRoll >= armorClass;
     if (!attackIsSuccessful) {
-      return { updatedTarget: target, message: 'Attack failed' };
+      message = this.appendMessage(message, 'Attack failed')
+      return { updatedTarget: target, message };
     }
     
     const damage = this.diceService.rollDice(DiceType.D4, 1);
     target.currentHealth = target.currentHealth - damage;
-    return { updatedTarget: target, message: `You attacked ${target.name} for ${damage} damage` };
+    message = this.appendMessage(message, `You attacked ${target.name} for ${damage} damage`);
+    return { updatedTarget: target, message };
   }
 
 
@@ -48,5 +55,9 @@ export class AttackService {
     const healing = this.diceService.rollDice(DiceType.D4, 2) + 2;
     target.health += healing;
     return { updatedTarget: target, message: `${target.name} drank a healing potion and gained ${healing} health` };
+  }
+
+  appendMessage(currentMessage: string, update: string): string {
+    return currentMessage ? `${currentMessage}\n\n${update}` : update;
   }
 }
